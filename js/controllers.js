7161a5f81ff4a52f0fe41979cd28021c275bff79
kmangutov <kmangutov@gmail.com>
2015-03-13 18:19:23 -0500

    everything done

--- js/controllers.js
+++ js/controllers.js
@@ -12,11 +12,71 @@ movieApp.config(['$routeProvider', function($routeProvider) {
       templateUrl: './partials/details.html',
       controller: 'DetailsController'
     })
+    .when('/gallery', {
+      templateUrl: './partials/gallery.html',
+      controller: 'GalleryController'
+    })
     .otherwise({
       redirectTo: '/list'
     });
 
 }]);
+
+movieApp.filter('selectedGenres', function() {
+
+  return function(movies, genres) {
+
+    console.log(JSON.stringify(genres));
+    if(genres.length == 0)
+      return movies;
+
+    return movies.filter(function(movie) {
+
+      for(var i in movie.genre) {
+
+        console.log(i);
+        if(genres.indexOf(movie.genre[i]) != -1)
+          return true;
+      }
+      return false;
+    });
+  }
+});
+
+movieApp.controller('GalleryController', ['$scope', '$http', '$location', 
+  function($scope, $http, $location) {
+
+    $scope.movies = [];
+    $scope.genres = [];
+    $scope.filters = [];
+
+    $http.get('./data/imdb250.json').success(function(data) {
+
+      for(var i = 0; i < data.length; i++) {
+        data[i].id = i;
+
+        var itemGenres = data[i].genre;
+
+        for(var j = 0; j < itemGenres.length; j++) {
+          var genre = itemGenres[j];
+          if($scope.genres.indexOf(genre) == -1)
+            $scope.genres.push(genre);
+        }
+      }
+
+      $scope.movies = data;
+    });
+
+
+
+    $scope.go = function(id) {
+
+      console.log("go to " + id);
+      $location.path("/details/" + id);
+    } 
+
+  }]);
+
 
 movieApp.controller('DetailsController', ['$scope', '$http', '$routeParams', 
   function($scope, $http, $routeParams) {
