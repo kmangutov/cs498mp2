5fbd0a35eaff4f840a8ec59dd426035272ff3826
kmangutov <kmangutov@gmail.com>
2015-03-13 17:38:04 -0500

    working next prev buttons, detail, list

--- js/controllers.js
+++ js/controllers.js
@@ -8,18 +8,52 @@ movieApp.config(['$routeProvider', function($routeProvider) {
       templateUrl: './partials/list.html',
       controller: 'ListController'
     })
+    .when('/details/:id', {
+      templateUrl: './partials/details.html',
+      controller: 'DetailsController'
+    })
     .otherwise({
       redirectTo: '/list'
     });
 
 }]);
 
-movieApp.controller('ListController', ['$scope', '$http', 
-  function($scope, $http) {
+movieApp.controller('DetailsController', ['$scope', '$http', '$routeParams', 
+  function($scope, $http, $routeParams) {
+
+    $scope.movie = {};
+    $scope.id = $routeParams.id;
+    $scope.movies_length = 0;
+
+    $http.get('./data/imdb250.json').success(function(data) {
+
+      $scope.movie = data[$routeParams.id];
+      $scope.movies_length = data.length;
+    });
+
+
+    $scope.go = function(id) {
+
+      console.log("go to " + id);
+      $location.path("/details/" + id);
+    } 
+
+    $scope.id_delta = function(delta) {
+
+      $location.path("/details/" + (id + delta));
+    }
+
+  }]);
+
+movieApp.controller('ListController', ['$scope', '$http', '$location', 
+  function($scope, $http, $location) {
 
     $scope.movies = [];
 
     $http.get('./data/imdb250.json').success(function(data) {
+
+      for(var i = 0; i < data.length; i++)
+        data[i].id = i;
 
       $scope.movies = data;
     });
@@ -29,9 +63,10 @@ movieApp.controller('ListController', ['$scope', '$http',
         descending: false
     };    
 
-    $scope.go = function(rank) {
+    $scope.go = function(id) {
 
-      console.log("go to " + rank);
+      console.log("go to " + id);
+      $location.path("/details/" + id);
     } 
 
     $scope.changeSorting = function(column) {
